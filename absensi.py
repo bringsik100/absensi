e40d4faea24a25e86d4ba900d330d54785f35828
Mamat bin Yanto <63628541+bringsik100@users.noreply.github.com>
2020-04-17 22:47:48 +0700

    Update absensi.py
    2020-04-17 output salah karena salah posisi iterasi employee dan tanggal

--- absensi.py
+++ absensi.py
@@ -2,7 +2,33 @@ import random
 import datetime
 
 #header
-header = 'NoPeg,No. Akun,No.,Nama,Auto-Assign,Tanggal,Jam Kerja,Awal tugas,Akhir tugas,Masuk,Keluar,Normal,Waktu real,Telat,Plg Awal,Bolos,Waktu Lembur,Waktu Kerja,Status,Hrs C/In,Hrs C/Out,Departemen,NDays,Akhir Pekan,Hari Libur,Lama Hadir,NDays_OT,Lembur A.Pekan,Libur Lembur'
+header = 'NoPeg,No. Akun,No.,Nama,Auto-Assign,Tanggal,Jam Kerja,Awal tugas,Akhir tugas,Masuk,Keluar,Normal,Waktu real,Telat,Plg Awal,Bolos,Waktu Lembur,Waktu Kerja,Status,Hrs C/In,Hrs C/Out,Departemen,NDays,Akhir Pekan,Hari Libur,Lama Hadir,NDays_OT,Lembur A.Pekan,Libur Lembur'.replace(',','\t')
+
+#misc
+''' Auto-Assign, Status, Hari Libur, Libur Lembur is empty'''
+empty = ' '
+norm_tab = '\"1'
+
+#employee
+employee = [['1','1','1','Ape','SSI'],
+			['2','2','2','Bull','SSI'],
+			['3','3','3','Cow','SSI'],
+			['4','4','4','Dog','SSI'],
+			['5','5','5','Elk','SSI'],
+			['6','6','6','Fowl','SSI']]
+
+nopeg = []
+noakun = []
+nomor = []
+nama = []
+departemen = []
+
+for i in employee:
+	nopeg.append(i[0])
+	noakun.append(i[1])
+	nomor.append(i[2])
+	nama.append(i[3])
+	departemen.append(i[4])
 
 #date time
 date_start = datetime.date(2018,12,1)
@@ -13,6 +39,8 @@ date_check_in = []
 date_check_out = []
 date_hour_in = []
 date_hour_out = []
+n_days = []
+w_days = []
 
 for i in range(date_delta.days+1):
 	x = date_start+datetime.timedelta(days=i)
@@ -25,11 +53,15 @@ for i in range(date_delta.days+1):
 	cot = [random.randint(16,18),random.randint(15,18)]
 	
 	if x.weekday() == 6:
+		n_days.append(' ')
+		w_days.append('\"1')
 		date_hour_in.append(datetime.datetime.combine(x,hour_off))
 		date_hour_out.append(datetime.datetime.combine(x,hour_off))
 		date_check_in.append(datetime.datetime.combine(x,hour_off))
 		date_check_out.append(datetime.datetime.combine(x,hour_off))
 	elif x.weekday() == 5:
+		n_days.append('\"1')
+		w_days.append(' ')
 		date_hour_in.append(datetime.datetime.combine(x,hour_in))
 		date_hour_out.append(datetime.datetime.combine(x,hour_out[1]))
 		
@@ -43,6 +75,8 @@ for i in range(date_delta.days+1):
 		date_check_out.append(datetime.datetime.combine(x,check_out))
 
 	else:
+		n_days.append('\"1')
+		w_days.append(' ')
 		date_hour_in.append(datetime.datetime.combine(x,hour_in))
 		date_hour_out.append(datetime.datetime.combine(x,hour_out[0]))
 		
@@ -55,25 +89,97 @@ for i in range(date_delta.days+1):
 		date_check_in.append(datetime.datetime.combine(x,check_in))
 		date_check_out.append(datetime.datetime.combine(x,check_out))
 
+#total attendance
+	total_att = []
+
+	for i in range(len(date_check_in)):
+		xx = date_check_out[i] - date_check_in[i]
+		total_att.append(xx)
+
 #title
-title='F:/abs-{}.txt'.format(date_finish)
+#title='F:/abs-{}.txt'.format(date_finish)
 
-#employee
-employee = [[1,1,1,'Ape'],[2,2,2,'Bull'],[3,3,3,'Cow'],[4,4,4,'Dog'],[5,5,5,'Elk'],[6,6,6,'Fowl']]
+#shift
+	shift_list = ['Senin-Jumat','Sabtu-Minggu']
+	sched_shift = []
 
-nopeg = []
-noakun = []
-nomor = []
-nama = []
+	for i in dates:
+		if i.weekday() == 5 or 6:
+			sched_shift.append(shift_list[1])
+		else:
+			sched_shift.append(shift_list[0])
 
-for i in employee:
-	nopeg.append(i[0])
-	noakun.append(i[1])
-	nomor.append(i[2])
-	nama.append(i[3])
+#normal column, real time column, & day out
+	time_real = []
+	day_out = []
 
-#misc
-auto = ' '
-status = ' '
-hari_libur = ' '
-libur_lembur = ' '
+	for i in date_check_in:
+		if i.hour == 0:
+			time_real.append(empty)
+			day_out.append('\"True\"')
+		else:
+			time_real.append('\"1')
+			day_out.append(empty)
+
+#late & overtime check in
+cin_late = []
+cin_over = []
+
+for i in range(len(date_check_in)):
+	if date_hour_in[i] < date_check_in[i]:
+		x = date_check_in[i] - date_hour_in[i]
+		cin_late.append(x)
+		cin_over.append(datetime.timedelta(0,0,0))
+	elif date_hour_in[i] > date_check_in[i]:
+		x = date_hour_in[i] - date_check_in[i]
+		cin_late.append(datetime.timedelta(0,0,0))
+		cin_over.append(x)
+	else:
+		cin_late.append(datetime.timedelta(0,0,0))
+		cin_over.append(datetime.timedelta(0,0,0))
+
+#early & overtime check out
+cot_early = [] 
+cot_over = []
+
+for i in range(len(date_check_out)):
+	if date_hour_out[i] > date_check_out[i]:
+		x = date_hour_out[i] - date_check_out[i]
+		cot_early.append(x)
+		cot_over.append(datetime.timedelta(0,0,0))
+	elif date_hour_out[i] > date_check_out[i]:
+		x = date_check_out[i] - date_hour_out[i]
+		cot_early.append(datetime.timedelta(0,0,0))
+		cot_over.append(x)
+	else:
+		cot_early.append(datetime.timedelta(0,0,0))
+		cot_over.append(datetime.timedelta(0,0,0))
+
+#total overtime
+total_over = []
+
+for i in range(len(cin_over)):
+	x = cin_over[i]+cot_over[i]
+	total_over.append(x)
+
+#work time
+work_hour = []
+
+for i in range(len(date_hour_in)):
+	x = date_hour_out[i] - date_hour_in[i]
+	y = x-(cin_late[i]-cot_early[i])
+	work_hour.append(y)
+
+#hrs check in, hrs check out, & departemen value
+
+t_val = '\"True\"'
+
+#printing result
+print(header)
+for i in range(len(employee)):
+	print('\n')
+	for x in range(len(dates)):
+		print('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\t{10}\t{11}\t{12}\t{13}\t{14}\t{15}\t{16}\t{17}\t{18}\t{19}\t{20}\t{21}\t{22}\t{23}\t{24}\t{25}\t{26}\t{27}\t{28}'.format(nopeg[i],
+		noakun[i],nomor[i],nama[i],empty,dates[x].isoformat(),sched_shift[i],date_hour_in[i].strftime('%H:%M'),date_hour_out[i].strftime('%H:%M'),date_check_in[i].strftime('%H:%M'),date_check_out[i].strftime('%H:%M'),norm_tab,time_real[i],cin_late[i],cot_early[i],
+		day_out[i],total_over[i],work_hour[i],empty,t_val,t_val,departemen[i],n_days[i],w_days[i],empty,total_att[i],empty,empty,empty))
+	
