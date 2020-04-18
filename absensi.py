892b1d0dee7b61d84dccae7f8f1d8b21dedca53c
Mamat bin Yanto <63628541+bringsik100@users.noreply.github.com>
2020-04-18 13:59:09 +0700

    Update absensi.py

--- absensi.py
+++ absensi.py
@@ -2,7 +2,7 @@ import random
 import datetime
 
 #header
-header = 'NoPeg,No. Akun,No.,Nama,Auto-Assign,Tanggal,Jam Kerja,Awal tugas,Akhir tugas,Masuk,Keluar,Normal,Waktu real,Telat,Plg Awal,Bolos,Waktu Lembur,Waktu Kerja,Status,Hrs C/In,Hrs C/Out,Departemen,NDays,Akhir Pekan,Hari Libur,Lama Hadir,NDays_OT,Lembur A.Pekan,Libur Lembur'.replace(',','\t')
+header = 'NoPeg,No. Akun,No.,Nama,Auto-Assign,Tanggal,Jam Kerja,Awal tugas,Akhir tugas,Masuk,Keluar,Normal,Waktu real,Telat,Plg Awal,Bolos,Waktu Lembur,Waktu Kerja,Status,Hrs C/In,Hrs C/Out,Departemen,NDays,Akhir Pekan,Hari Libur,		Lama Hadir,NDays_OT,Lembur A.Pekan,Libur Lembur'.replace(',','\t')
 
 #misc
 ''' Auto-Assign, Status, Hari Libur, Libur Lembur is empty'''
@@ -34,15 +34,45 @@ hour_off = datetime.time(0,0,0)
 cin = random.randint(7,8)
 cot = [random.randint(16,18),random.randint(15,18)]
 
-def dates(a)
+#globals
+c_in = []
+c_out = []
+h_in = []
+h_out = []
+n_days = []
+w_days = []
+t_att = []
+s_shift = []
+t_real = []
+d_out = []
+cin_late = []
+cin_over = []
+cot_early = [] 
+cot_over = []
+total_over = []
+w_hour = []
+
+#date module
+def dates(a):
 	#a is a date_list
-	c_in = []
-	c_out = []
-	h_in = []
-	h_out = []
-	n_days = []
-	w_days = []
-	for i in range(a):
+	global c_in
+	global c_out
+	global h_in
+	global h_out
+	global n_days
+	global w_days
+	global t_att
+	global s_shift
+	global t_real
+	global d_out
+	global cin_late
+	global cin_over
+	global cot_early
+	global cot_over
+	global total_over
+	global w_hour
+	
+	for i in a:
 		if i.weekday() == 6:
 			n_days.append(' ')
 			w_days.append('\"1')
@@ -50,12 +80,12 @@ def dates(a)
 			h_out.append(datetime.datetime.combine(i,hour_off))
 			c_in.append(datetime.datetime.combine(i,hour_off))
 			c_out.append(datetime.datetime.combine(i,hour_off))
-		elif x.weekday() == 5:
+		
+		elif i.weekday() == 5:
 			n_days.append('\"1')
 			w_days.append(' ')
 			h_in.append(datetime.datetime.combine(i,hour_in))
 			h_out.append(datetime.datetime.combine(i,hour_out[1]))
-		
 			if cin == 8:
 				check_in = datetime.time(cin,random.randint(0,15))
 				check_out = datetime.time(cot[1],random.randint(0,59))
@@ -70,7 +100,6 @@ def dates(a)
 			w_days.append(' ')
 			h_in.append(datetime.datetime.combine(i,hour_in))
 			h_out.append(datetime.datetime.combine(i,hour_out[0]))
-		
 			if cin == 8:
 				check_in = datetime.time(cin,random.randint(0,15))
 				check_out = datetime.time(cot[1],random.randint(0,59))
@@ -81,15 +110,12 @@ def dates(a)
 			c_out.append(datetime.datetime.combine(i,check_out))
 
 	#total attendance
-	t_att = []
-	for x in range(len(date_check_in)):
+	for x in range(len(c_in)):
 		xx = c_out[x] - c_in[x]
 		t_att.append(xx)
 
 	#shift
 	shift_l = ['Senin-Jumat','Sabtu-Minggu']
-	s_shift = []
-
 	for x in a:
 		if x.weekday() == 5 or 6:
 			s_shift.append(shift_l[1])
@@ -97,22 +123,16 @@ def dates(a)
 			s_shift.append(shift_l[0])
 
 	#normal column, real time column, & day out
-	t_real = []
-	d_out = []
-
 	for x in c_in:
-		if i.hour == 0:
+		if x.hour == 0:
 			t_real.append(empty)
 			d_out.append('\"True\"')
 		else:
-			t_real.append('\"1')
+			t_real.append(norm_tab)
 			d_out.append(empty)
 
 	#late & overtime check in
-	cin_late = []
-	cin_over = []
-
-	for X in range(len(c_in)):
+	for x in range(len(c_in)):
 		if h_in[x] < c_in[x]:
 			y = c_in[x] - h_in[x]
 			cin_late.append(y)
@@ -126,8 +146,6 @@ def dates(a)
 			cin_over.append(datetime.timedelta(0,0,0))
 
 	#early & overtime check out
-	cot_early = [] 
-	cot_over = []
 
 	for i in range(len(c_out)):
 		if h_out[i] > c_out[i]:
@@ -142,31 +160,28 @@ def dates(a)
 			cot_early.append(datetime.timedelta(0,0,0))
 			cot_over.append(datetime.timedelta(0,0,0))
 
-#total overtime
-total_over = []
+	#total overtime
+	for i in range(len(cin_over)):
+		x = cin_over[i]+cot_over[i]
+		total_over.append(x)
 
-for i in range(len(cin_over)):
-	x = cin_over[i]+cot_over[i]
-	total_over.append(x)
-
-#work time
-work_hour = []
-
-for i in range(len(date_hour_in)):
-	x = h_out[i] - date_hour_in[i]
-	y = x-(cin_late[i]-cot_early[i])
-	work_hour.append(y)
+	#work time
+	for i in range(len(h_in)):
+		x = h_out[i] - h_in[i]
+		y = x-(cin_late[i]-cot_early[i])
+		w_hour.append(y)
 
 #hrs check in, hrs check out, & departemen value
-
 t_val = '\"True\"'
+
 #title
 #title='F:/abs-{}.txt'.format(date_finish)
+
 #printing result
+
 print(header)
-for i in range(len(employee)):
-	print('\n')
-	for x in range(len(dates)):
-		print('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\t{10}\t{11}\t{12}\t{13}\t{14}\t{15}\t{16}\t{17}\t{18}\t{19}\t{20}\t{21}\t{22}\t{23}\t{24}\t{25}\t{26}\t{27}\t{28}'.format(nopeg[i],
-		noakun[i],nomor[i],nama[i],empty,dates[x].isoformat(),sched_shift[i],date_hour_in[i].strftime('%H:%M'),h_out[i].strftime('%H:%M'),date_check_in[i].strftime('%H:%M'),c_out[i].strftime('%H:%M'),norm_tab,time_real[i],cin_late[i],cot_early[i],
-		day_out[i],total_over[i],work_hour[i],empty,t_val,t_val,departemen[i],n_days[i],w_days[i],empty,total_att[i],empty,empty,empty))
+for i in range(len(nama)):
+	dates(date_list)
+	for x in range(len(date_list)):
+		print (nopeg[i],noakun[i],nomor[i],nama[i],empty,date_list[i],s_shift[i],h_in[i],h_out[i],c_in[i],c_out[i],norm_tab,t_real[i],cin_late[i],cot_early[i],d_out[i],total_over[i],w_hour[i],empty,t_val,t_val,departemen[i],n_days[i],w_days[i],empty,w_hour[i],empty,empty,empty)
+		
