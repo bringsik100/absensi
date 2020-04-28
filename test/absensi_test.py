9ad62e2c2b63328ba95a7268154a6753cb74a957
Mamat bin Yanto <63628541+bringsik100@users.noreply.github.com>
2020-04-28 16:33:29 +0700

    Update absensi_test.py

--- test/absensi_test.py
+++ test/absensi_test.py
@@ -53,54 +53,8 @@ def getd(arg):
 mulai = dt(2020,10,10,0) #getd(' masukkan tanggal awal absensi: ')
 akhir = dt(2020,10,20,0) #getd(' masukkan tanggal akhir absensi: ')
 
+test.test_datecount(mulai,akhir)
+test.test_proses()
 
-jadwal = test.proses_test.test_datecount(mulai,akhir)
-
-waktu = test.proses_test.test_timecount
-
-daftar_peg = test.pegawai_test.test_sumber.read()
-
-def ngetes(*args):
-	for i in args:
-		print(i)
-
-for pegawai in daftar_peg:
-
-	for tanggal in jadwal.dates:
-	
-		if tanggal.weekday == 6:
-			jam_masuk = td(0)
-			jam_keluar = td(0)
-			cek_masuk = td(0)
-			cek_keluar = td(0)
-		
-			waktu(jam_masuk,jam_keluar,cek_masuk,cek_keluar)
-			
-			ngetes(waktu.hour_in
-				,waktu.hour_out
-				,waktu.check_in
-				,waktu.check_out)
-	
-		elif tanggal.weekday == 5:
-			jam_masuk = td(hours=8)
-			jam_keluar = td(hours=15)
-			cek_masuk = td(hours = ri(7,9), minutes = ri(0,59), seconds = ri(0,59))
-			cek_keluar = td(hours = ri(14,18), minutes = ri(0,59), seconds = ri(0,59))
-		
-			waktu(jam_masuk,jam_keluar,cek_masuk,cek_keluar)
-			ngetes(waktu.hour_in
-				,waktu.hour_out
-				,waktu.check_in
-				,waktu.check_out)
-	
-		else:
-			jam_masuk = td(hours=8)
-			jam_keluar = td(hours=16)
-			cek_masuk = td(hours = ri(7,9), minutes = ri(0,59), seconds = ri(0,59))
-			cek_keluar = td(hours = ri(14,18), minutes = ri(0,59), seconds = ri(0,59))
-		
-			waktu(jam_masuk,jam_keluar,cek_masuk,cek_keluar)
-			ngetes(waktu.hour_in
-				,waktu.hour_out
-				,waktu.check_in
-				,waktu.check_out)
+if __name__=='__main__':
+	main()
