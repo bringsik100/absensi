53777838fbe08c182716f459ac22b678aeaf9fdb
Cak Sur <63628541+bringsik100@users.noreply.github.com>
2020-04-30 09:29:37 +0700

    Add files via upload
    fix fix fuck

--- test/absensi_test.py
+++ test/absensi_test.py
@@ -108,46 +108,46 @@ def test_process(start,end,buffer):
 	"""buffer berfungsi sebagai penampung data"""
 	
 	"""ambil library yang dibutuhkan"""
-	employee = test_get_data('test/data/pegawai.json')
-	schedule = test_get_data('test/data/jadwal.json')
-	subdata = test_get_data('test/data/judul.json')
-	holiday = list(test_get_data('test/data/libur.json'))
+	employee = test_get_data('data/pegawai.json')
+	schedule = test_get_data('data/jadwal.json')
+	subdata = test_get_data('data/judul.json')
+	holiday = list(test_get_data('data/libur.json'))
 	
 	"""daftar tanggal"""
 	delta = end - start
 	
 	"""looping pegawai"""
 	for x in range(len(employee)):
-
+		x = x + 1
 		"""looping tanggal"""
 		for day in range(delta.days+1):
 			
 			"""tentukan tangal"""
-			thisday = start + day
+			thisday = start + td(days = day)
 			"""isi data tanggal dan data pegawai"""
-			data = judul
+			data = subdata
 			data["Tanggal"] = thisday.strftime('%Y-%m-%d')
-			data["NoPeg"] = employee[x]['nopeg']
-			data["No. Akun"] = employee[x]['akun']
-			data["No."] = employee[x]['nomor']
-			data["Nama"] = employee[x]['nama']
+			data["NoPeg"] = employee[str(x)]['nopeg']
+			data["No. Akun"] = employee[str(x)]['akun']
+			data["No."] = employee[str(x)]['nomor']
+			data["Nama"] = employee[str(x)]['nama']
 			data["Auto-Assign"] = '0'
 			data["Status"] = ' '
 			data["Hrs C/In"] = ' '
 			data["Hrs C/Out"] = ' '
-			data["Departemen"] = employee[x]['dept']
+			data["Departemen"] = employee[str(x)]['dpt']
 			data["Waktu real"] = '1'
 			
 			"""fungsi untuk mengisi waktu"""
-			if date in holiday:
+			if thisday in holiday:
 				"""hari libur"""
-				thisday_schedule = data["Jam Kerja"] = schedule[0]['name']
-				hour_in = data["Awal tugas"] = test_gethour(schedule[0]['hour start'])
-				hour_out = data["Akhir tugas"] = test_gethour(schedule[0]['hour end'])
+				thisday_schedule = data["Jam Kerja"] = schedule["0"]["name"]
+				hour_in = data["Awal tugas"] = test_gethour(schedule["0"]['hour start'])
+				hour_out = data["Akhir tugas"] = test_gethour(schedule["0"]['hour end'])
 				check_in = data["Masuk"] = td(seconds = 0)
 				check_out = data["Keluar"] = td(seconds = 0)
-				late_in = data["Telat"] = test_late_in(hour_in,check_in,test_gethour(schedule[0]['late in']))
-				early_out = data["Plg Awal"] = test_early_out(hour_out,check_out,test_gethour(schedule[0]['early out']))
+				late_in = data["Telat"] = test_late_in(hour_in,check_in,test_gethour(schedule["0"]['late in']))
+				early_out = data["Plg Awal"] = test_early_out(hour_out,check_out,test_gethour(schedule["0"]['early out']))
 				overtime = data["Waktu Lembur"] = test_overtime(hour_in,hour_out,check_in,check_out)
 				worktime = data["Waktu Kerja"] = test_worktime(hour_in,hour_out,check_in,check_out)
 				totaltime = data["Lama Hadir"] = test_totaltime(check_in,check_out)
@@ -157,14 +157,14 @@ def test_process(start,end,buffer):
 				data["Hari Libur"] = '0'
 				data["NDays_OT"] = '0'
 				data["Lembur A.Pekan"] = '0'
-				if check_in == td(seconds = 0) or late_in > test_gethour(schedule[0]["checkin max"]) or check_out < test_gethour(schedule[0]["checkin max"]):
+				if check_in == td(seconds = 0) or late_in > test_gethour(schedule["0"]["checkin max"]) or check_out < test_gethour(schedule["0"]["checkin max"]):
 					data["Bolos"] = '1'
 				else : 
 					data["Bolos"] = '0'	
 			else:
-				if date.weekday == 6:
+				if thisday.weekday == 6:
 					"""hari minggu """
-					thisday_schedule = data["Jam Kerja"] = schedule[2]['name']
+					thisday_schedule = data["Jam Kerja"] = schedule[2]["name"]
 					hour_in = data["Awal tugas"] = test_gethour(schedule[2]['hour start'])
 					hour_out = data["Akhir tugas"] = test_gethour(schedule[2]['hour end'])
 					check_in = data["Masuk"] = check_in = td(hours = ri(7,8))
@@ -187,9 +187,9 @@ def test_process(start,end,buffer):
 					else : 
 						data["Bolos"] = '0'	
 		
-				elif date.weekday == 5:
+				elif thisday.weekday == 5:
 					"""hari sabtu """
-					thisday_schedule = data["Jam Kerja"] = schedule[2]['name']
+					thisday_schedule = data["Jam Kerja"] = schedule[2]["name"]
 					hour_in = data["Awal tugas"] = test_gethour(schedule[2]['hour start'])
 					hour_out = data["Akhir tugas"] = test_gethour(schedule[2]['hour end'])
 					check_in = data["Masuk"] = check_in = td(hours = ri(7,8))
@@ -213,7 +213,7 @@ def test_process(start,end,buffer):
 						data["Bolos"] = '0'	
 				else:
 					"""hari senin sampai jumat"""
-					thisday_schedule = data["Jam Kerja"] = schedule[1]['name']
+					thisday_schedule = data["Jam Kerja"] = schedule[1]["name"]
 					hour_in = data["Awal tugas"] = test_gethour(schedule[1]['hour start'])
 					hour_out = data["Akhir tugas"] = test_gethour(schedule[1]['hour end'])
 					check_in = data["Masuk"] = check_in = td(hours = ri(7,8))
@@ -251,7 +251,7 @@ def test_print(buffer):
 		print(i)
 
 
-def test_excell(title,buffer):
+def test_excell(file_title,buffer):
 	"""output ke excell"""
 	
 	"""membaca data header dari judul.json"""
@@ -260,7 +260,7 @@ def test_excell(title,buffer):
 	
 	book = Workbook()
 	sheet = self.book.active
-	sheet.title = title
+	sheet.title = file_title
 	"""daftar kolom untuk excell ouput"""
 	self.column = list(string.ascii_uppercase)+['AA','AB','AC']
 	"""header untuk mengisi baris pertama"""
@@ -274,32 +274,31 @@ def test_excell(title,buffer):
 			self.sheet.cell(column=y+1,row=x+2,value=(z[y]))
 
 	"""save ke excell"""
-	wb.save('{}.xlsx'.format(title))
+	wb.save('{}.xlsx'.format(file_title))
 	wb.close()
 
-def test_json(title,buffer):
+def test_json(file_title,buffer):
 	"""output ke json"""
-	with open('{}.json'.format(title),'w') as jsonfile:
+	with open('{}.json'.format(file_title),'w') as jsonfile:
 		json.dump(buffer)
 
-def test_csv(title,buffer):
+def test_csv(file_title,buffer):
 	"""output ke csv"""
-	self.title = title
-	with open('{}.csv'.format(title), 'w', dialect='excell', newline='') as csvfile:
+	with open('{}.csv'.format(file_title), 'w', dialect='excell', newline='') as csvfile:
 		x = csv.writer(csvfile, delimiter=',',quotechar='"')
 		for i in buffer:
 			x.writerow(i)
 
-def test_txt(title,buffer):
+def test_txt(file_title,buffer):
 	"""output ke text"""
-	with open('{}.txt'.format(title), 'w') as txtfile:
+	with open('{}.txt'.format(file_title), 'w') as txtfile:
 		txtfile.write(buffer)
 
-def test_option(title,func,buffer):
-	if title == None:
-		title = dt.today.strftime('%Y-%m-%d.%H.%M.%s')
+def test_option(file_title,func,buffer):
+	if file_title == None:
+		file_title = dt.today().strftime('%Y-%m-%d.%H.%M.%s')
 		
-	func(title,buffer)
+	func(file_title,buffer)
 
 """salam pembuka"""
 print("""
@@ -309,10 +308,10 @@ MODUL PENGISI ABSENSI OTOMATIS
 
 def main():
 	"""mengambil data pegawai,jadwal,judul,libur"""
-	pegawai = get_data('test/data/pegawai.json')
-	jadwal = get_data('test/data/jadwal.json')
-	judul = get_data('test/data/judul.json')
-	libur = get_data('test/data/libur.json')
+	pegawai = test_get_data('data/pegawai.json')
+	jadwal = test_get_data('data/jadwal.json')
+	judul = test_get_data('data/judul.json')
+	libur = test_get_data('data/libur.json')
 
 	print("""
 	metode pengisian :
@@ -323,14 +322,14 @@ def main():
 	""")
 	
 	"""tanya tanggal awal dan akhir otomatis"""
-	awal = dt(2020,10,10,0) #getd(' masukkan tanggal awal absensi: ')
+	awal = dt(2020,10,10,0) #getd(' masukkan tanggal awal absensi: ') 
 	akhir = dt(2020,10,20,0) #getd(' masukkan tanggal akhir absensi: ')
 	hasil = []
 	
 	"""tanya tanggal awal dan akhir lewat input (nonaktif)
 	awal = test_get_input(' masukkan tanggal awal absensi: ')
 	akhir = test_get_input(' masukkan tanggal akhir absensi: ')
-	"""
+	""" 
 	
 	"""main proses"""
 	test_process(awal,akhir,hasil)
