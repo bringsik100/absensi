8964a22cb4cfee81fb8d80ccbb8a4c082337ba34
bringsik100 <63628541+bringsik100@users.noreply.github.com>
2020-05-02 22:36:15 +0700

    edit pegawai.json

--- absensi/pegawai.py
+++ absensi/pegawai.py
@@ -3,50 +3,66 @@
 Pegawai
 """
 
-
-'''
-fungsi untuk create, read, write, edit, dan delete item dalam employee.json
-'''
+"""
+- buat perintah Read untuk membaca data dan menampungnya ke dalam buffer
+- buat perintah View untuk menampilkan buffer ke layar
+- buat perintah Find untuk mencari data di dalam buffer
+- buat perintah Edit untuk menyunting data di dalam buffer
+- buat perintah Create untuk menambahkan data ke dalam buffer
+- buat perintah Delete untuk menghapus data di dalam buffer
+- buat perintah Write untuk menuliskan buffer ke dalam data
+"""
+"""import library"""
 import json
 
-class test_sumber:
-	'''fungsi untuk membaca pegawai.json'''
+class Emp:
+	'''
+	fungsi untuk create, read, write, edit, dan delete item dalam employee.json
+	'''
 	def __init__(self):
-		'''membaca input'''
-		with open('test/data/pegawai.json','r') as srce:
-			return json.load(srce)
+		'''membaca file pegawai.json'''
+		with open('data/pegawai.json') as source:
+			self.buffer = json.load(source)
+			return self.buffer
 
-	def test_print(self):
-		print(self.source)
-		
-class test_masuk:
-	'''fungsi untuk memasukkan data ke pegawai.json'''
-	def __init__(self,**kwargs):
-		'''input dari user'''
-		self.nopeg = nopeg
-		self.akun = akun
-		self.nomor = nomor
-		self.nama = nama
-		self.dept = dept
-		self.buffer = {}
-		
-		'''data ditampung di buffer dan ditampilkan di layar'''
-		return self.buffer.update(self.nopeg, self.akun, self.nomor, self.nama, self.dept)
+	def view_all(self):
+		"""tampilkan data di layar"""
 		print(self.buffer)
+
+	def view_id(self,pid):
+		"""tampilkan data pegawai dari pilihan id"""
+		print(self.data[str(pid)])
 	
-	def test_print_all(self):
-		''' menampilkan data source dan buffer ke layar'''
-		print(test_sumber.source + self.buffer)
+	def find_elem(self,arg):
+		"""mencari data pegawai dengan kata kunci"""
+		for elem in self.data:
+			if args in self.data[elem]:
+				print(f'{arg} ada di dalam {self.data.[elem]}')
+			else:
+				print(f'data {arg} tidak ditemukan')
+
+	def edit_elem(self,pid,**kwargs):
+		"""menyunting data dari buffer"""
+		self.buffer.update{[pid]:{kwargs}}
 	
-	def test_print_buff(self):
-		''' menampilkan data buffer ke layar'''
-		print(self.buffer)
-		
-	def test_write(self):
-		'''menulis isi source dan buffer ke pegawai.json'''
-		with open('data/pegawai.json','w') as srce:
-			x = json.dumps(test_sumber.source + self.buffer)
-			srce.write(x)
+	def Add_elem(self,**kwargs):
+		""" menambahkan data ke dalam buffer"""
+		self.new_pid = str(len(self.buffer+1))
+		self.buffer.update{self.new_pid:{kwargs}}
+
+	def Delete(self,pid):
+		"""menghapus data di dalam buffer"""
+		self.pid = pid
+		del self.buffer[self.pid]
+	
+	def Write(self):
+		""" menulis data dari buffer ke dalam pegawai.json"""
+		with open('data/pegawai.json','w') as source:
+			source.write(json.dump(self.buffer))
+
+def main():
+	"""modul utama"""
+	print("not ready yet")
 
 if __name__ == '__main__':
 	main()
