b4c9fbc9758355d32159eba75fcce5c4298e4921
bringsik100 <63628541+bringsik100@users.noreply.github.com>
2020-05-03 21:47:20 +0700

    tambah HOME folder ke output

--- absensi/output.py
+++ absensi/output.py
@@ -8,6 +8,14 @@ import json
 import csv
 import string
 from openpyxl import Workbook
+import os
+
+"""mencari home direktori"""
+def home():
+	if os.name == 'nt':
+		return os.environ["HOME"]+"/My Documents/"
+	else:
+		return os.environ["HOME"]+"/user/"
 
 """fungsi ouput dengan 5 pilihan : layar, excell, JSON, csv, text"""
 
@@ -37,24 +45,24 @@ def pt_excell(file_title,buffer):
 			sheet.cell(column=cols+1,row=rows+2,value=buffer[rows][str(cols)])
 		
 	"""save ke excell"""
-	book.save('{}.xlsx'.format(file_title))
+	book.save('{}.xlsx'.format(home()+file_title))
 	book.close()
 
 def pt_json(file_title,buffer):
 	"""output ke json"""
-	with open('{}.json'.format(file_title),'w') as jsonfile:
+	with open('{}.json'.format(home()+file_title),'w') as jsonfile:
 		json.dumps(buffer)
 
 def pt_csv(file_title,buffer):
 	"""output ke csv"""
-	with open(f'{file_title}.csv', 'w', newline='') as csvfile:
+	with open(f'{home()+file_title}.csv', 'w', newline='') as csvfile:
 		x = csv.writer(csvfile, delimiter=',',quotechar='"')
 		for i in buffer:
 			x.writerow(i)
 
 def pt_txt(file_title,buffer):
 	"""output ke text"""
-	with open('{}.txt'.format(file_title), 'w') as txtfile:
+	with open('{}.txt'.format(home()+file_title), 'w') as txtfile:
 			txtfile.write(json.dumps(buffer))
 
 def main():
