123bb873f0780c0cf9e46fdc539a825c9c5abc92
Cak Sur <63628541+bringsik100@users.noreply.github.com>
2020-05-08 21:38:47 +0700

    pakai class

--- absensi/getdata.py
+++ absensi/getdata.py
@@ -32,93 +32,124 @@ def cnxn():
 		print(f'koneksi gagal\n{sys.exc_info()} \n')
 		pass
 
+class CreateTable:
+	'''modul untuk membuat tabel'''
+	def __init__(self,table_name,*args):
+		''' ambil judul tabel dan judul kolom'''
+		self.table_name = table_name
+		self.args = args
+		try:
+			self.conn = db.connect('data/report.db')
+			print('koneksi berhasil')
+		except Exception:
+			print(f'koneksi gagal\n{sys.exc_info()} \n')
+			pass
+
+	def create(self):
+		try:
+			self.conn.execute('''create table if not exist {self.table_name}{self.args}''')
+			conn.commit()
+			print('tabel {self.table_name} berhasil dibuat')
+		except Exception:
+			print(f'tabel {self.table_name} gagal dibuat karena:\n{sys.exc_info()} \n') 
+
+class EditTable:
+	
+	def __init__(self,table_name):
+		self.table_name = table_name
+
+		try:
+			conn=db.connect('data/report.db')
+			print('koneksi berhasil')
+		except Exception:
+			print(f'koneksi gagal\n{sys.exc_info()} \n')
+			pass
+	
+	def this_input(self,input_args):
+		self.input_args = input_args
+		cari = input(self.input_args)
+		return cari
+	
+	def view_all(self):
+		"""lihat semua data """
+		self.view = conn.execute('select * from {self.table_name};')
+		return self.view
+	
+	def add_column(self.*adds):
+		self.adds = adds
+	
+	def add_values(self,*advs):
+		self.advs = advs
+		for i in range(len(self.advs)):
+			if self.advs[i] == None or self.advs[i] == "":
+				return self.advs[i] = ""
+			else:
+				return self.advs[i] = self.advs[i]
+	
+	def add_func(self):
+		try:
+			self.conn.execute(f'''insert into {self.table_name}{self.add_column()} values(self.add_values());''')
+			self.conn.commit()
+			print(f'\ndata {self.table_name} telah masuk ke database \n')
+		except Exception:
+			print(f'\ndata {self.table_name} gagal masuk karena:\n{sys.exc_info()}\n') 
+
+	def edit_column(self.*editc):
+		self.editc = editc
+	
+	def edit_values(self,*new_val):
+		self.new_val = new_val
+		
+	def edit_func(self):
+		find = self.this_input()
+		old_values = conn.execute(f'select from pegawai where noID={find};')
+		new_values = self.edit_values
+		
+		for i in range(len(data_lama)):
+			if new_values[i] == None or new_values[i] == "":
+				return old_values[i]
+			else:
+				return new_values[i]
+		print('ubah data pegawai? \t')
+		sure = self.this_input('Ya atau Tidak \n')
+		if sure.lower == 'y':
+			try:
+				conn.execute(f'''update {self.table_name} where noID={find} values = {new_values}''')
+				print(f'\ndata {self.table_name} berhasil diubah \n')
+			except Exception:
+				print(f'\ndata {self.table_name} gagal diubah karena:\n{sys.exc_info()}\n') 
+	
+	def delete(self):
+		find = self.this_input()
+		try:
+			cursor=conn.execute(f'select from {self.table_name} where noID={find};')
+			print('hapus data pegawai? \t')
+			sure = input('Ya atau Tidak \n')
+			if sure.lower == 'y':
+				try:
+					execute(f'delete from {table_name} where noID={find};')
+					print('data {self.table_name} urutan ke {i} telah dihapus \n')
+				except Exception:
+					cls()
+					print(f'\ndata {self.table_name} tidak bisa dihapus karena:\n{sys.exc_info()}\n') 
+			else:
+				cls()
+				print('perintah dibatalkan \n')
+
+tab_pegawai = CreateTable('pegawai','noID integer primary key autoincrement','nopeg text','noakun text','nokartu text','nama text','titel text','departemen text')
+
+tab_jadwal = CreateTable('jadwal','noID integer primary key autoincrement','nama text','`Jam Masuk` text','`Jam Keluar` text','`Telat` text','`Pulang Cepat` text','`Harus CIn` text','`Harus COut` text','`Normal` text','`Akhir Pekan` text','`Hari Libur` text','`Waktu Real` text')
+
+tab_liburan = CreateTable('liburan','tanggal text','hari text')
+
+tab_laporan = CreateTable('laporan','row_id integer primary key autoincrement','`NoPeg` text','`No. Akun` text','`No.` text','`Nama` text','`Auto-Assign` text','`Tanggal` text','`Jam Kerja` text','`Awal tugas` text','`Akhir tugas` text','`Masuk` text','`Keluar` text','`Normal` text','`Waktu real` text','`Telat` text','`Plg Awal` text','`Bolos` text','`Waktu Lembur` text','`Waktu Kerja` text','`Status` text','`Hrs C/In` text','`Hrs C/Out` text','`Departemen` text','`NDays` text','`Akhir Pekan` text','`Hari Libur` text','`Lama Hadir` text','`NDays_OT` text','`Lembur A.Pekan` text','`Libur Lembur` text')
+
 def recover():
 	"""recover tabel"""
-	def tab_pegawai():
-		try:
-			conn.execute('''create table if not exist pegawai(
-			noID integer primary key autoincrement
-			,nopeg text
-			,noakun text
-			,nokartu text
-			,nama text
-			,titel text
-			,departemen text)
-			''')
-			conn.commit()
-			print('tabel pegawai berhasil dibuat')
-		except Exception:
-			print(f'tabel pegawai gagal dibuat karena:\n{sys.exc_info()} \n') 
-	
-	def tab_jadwal():
-		try:
-			conn.execute('''create table if not exist jadwal(
-			noID integer primary key autoincrement
-			,nama text
-			,`Jam Masuk` text
-			,`Jam Keluar` text
-			,`Telat` text
-			,`Pulang Cepat` text
-			,`Harus CIn` text
-			,`Harus COut` text
-			,`Normal` text
-			,`Akhir Pekan` text
-			,`Hari Libur` text
-			,`Waktu Real` text)
-			''')
-			conn.commit()
-			print('tabel jadwal berhasil dibuat')
-		except Exception:
-			print(f'tabel jadwal gagal dibuat karena:\n{sys.exc_info()} \n')
-	def tab_liburan():
-		try:
-			conn.execute('''create table if not exist liburan(
-			tanggal text
-			,hari text)
-			''')
-			conn.commit()
-			print('tabel liburan berhasil dibuat')
-		except Exception:
-			print(f'tabel liburan gagal dibuat karena:\n{sys.exc_info()} \n') 
-	
-	def tab_laporan():
-		try:
-			"""buat table baru di database"""
-			conn = db.connect("data/report.db")
-			conn.execute("""create table if not exists absensi (row_id integer primary key autoincrement
-			,`NoPeg` text
-			,`No. Akun` text
-			,`No.` text
-			,`Nama` text
-			,`Auto-Assign` text
-			,`Tanggal` text
-			,`Jam Kerja` text
-			,`Awal tugas` text
-			,`Akhir tugas` text
-			,`Masuk` text
-			,`Keluar` text
-			,`Normal` text
-			,`Waktu real` text
-			,`Telat` text
-			,`Plg Awal` text
-			,`Bolos` text
-			,`Waktu Lembur` text
-			,`Waktu Kerja` text
-			,`Status` text
-			,`Hrs C/In` text
-			,`Hrs C/Out` text
-			,`Departemen` text
-			,`NDays` text
-			,`Akhir Pekan` text
-			,`Hari Libur` text
-			,`Lama Hadir` text
-			,`NDays_OT` text
-			,`Lembur A.Pekan` text
-			,`Libur Lembur` text);""")
-			conn.commit()
-			print('tabel laporan berhasil dibuat')
-		except Exception:
-			print(f'tabel laporan gagal dibuat karena:\n{sys.exc_info()} \n') 
+	tab_pegawai
+	tab_jadwal
+	tab_liburan
+	tab_laporan
 
 def vari_bl(baru,lama):
 	"""nilai var baru dan lama"""
@@ -133,6 +164,11 @@ def vari_null(var):
 		return ""
 	else:
 		return var
+
+#ubah_pegawai = EditTable('pegawai')
+#ubah_jadwal = EditTable('jadwal')
+#ubah_libur = EditTable('libur')
+#ubah_laporan = EditTable('laporan')
 
 def ubah_pegawai():
 	cnxn()
