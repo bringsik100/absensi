33726a0d8b97c325079cc01d7fef1a76ef62e079
Cak Sur <63628541+bringsik100@users.noreply.github.com>
2020-05-08 15:08:11 +0700

    update absensi

--- absensi/absensi.py
+++ absensi/absensi.py
@@ -65,111 +65,192 @@ def view_tabel(nama_tabel):
 	except Exception:
 		print(f'gagal menampilkan isi tabel karena :\n{sys.exc_info()}\n')
 
-"""salam pembuka"""
-print("""
-SELAMAT DATANG DI ABTOMATIS
-MODUL PENGISI ABSENSI OTOMATIS
-""")
-
-def main():
-	"""fungsi utama, semua data diproses disini"""
-	
+def buat():
+	"""modul untuk mengisi absen otomatis"""
+	cls()
 	print("""
-	metode pengisian :
-	masukkan tanggal awal dan akhir dengan format YYYY-MM-DD dimana 
-	YYYY = 4 angka tahun
-	MM = 2 angka bulan 
-	DD = 2 angka tanggal
-	""")
-	
-	"""tanya tanggal awal dan akhir otomatis"""
-	awal = proses.get_input(' masukkan tanggal awal absensi: ') 
-	akhir = proses.get_input(' masukkan tanggal akhir absensi: ')
-	hasil = []
-	
-	"""main proses"""
-	"""proses ada di dalam modul proses"""
-	proses.process(awal,akhir,hasil)
-	"""otomatis menyimpan ke database"""
-	try:
-		output.pt_db(hasil)
-	except Exception:
-		print(f"gagal menyimpan ke database karena : \n{Exception}\n")
-		print(f"laporan dari system : \n{sys.exc_info()}")
-	
-	def this_output():
-		"""memproses data dari penampung untuk di cetak ke layar atau berkas"""
-		
-		print("""
-		pilih format output dari 5 opsi
-		 0 = layar
-		 1 = excel
-		 2 = JSON
-		 3 = csv
-		 4 = text
+		metode pengisian :
+		masukkan tanggal awal dan akhir dengan format YYYY-MM-DD dimana 
+		YYYY = 4 angka tahun
+		MM = 2 angka bulan 
+		DD = 2 angka tanggal
 		""")
-		"""pilih opsi output"""
-		opsi = str(input (" opsi : "))
-		
-		if opsi =='0':
-			"""cetak ke layar"""
-			output.pt_screen(hasil)
-		
-		elif opsi == '1':
-			"""cetak ke excell"""
-			judul_opsi = input("beri judul : " )
-			if judul_opsi == None:
-				judul_blank = dt.today.strftime('%Y-%m-%d-%H.%M.%s')
-				ouput.pt_excell(judul_blank,hasil)
-			else:
-				ouput.pt_excell(judul_opsi,hasil)
-		
-		elif opsi == '2':
-			"""cetak ke JSON"""
-			judul_opsi = input("beri judul : " )
-			if judul_opsi == None:
-				judul_blank = dt.today.strftime('%Y-%m-%d-%H.%M.%s')
-				ouput.pt_json(judul_blank,hasil)
-			else:
-				ouput.pt_json(judul_opsi,hasil)
-		
-		elif 'opsi' == '3':
-			"""cetak ke csv"""
-			judul_opsi = input("beri judul : " )
-			if judul_opsi == None:
-				judul_blank = dt.today.strftime('%Y-%m-%d-%H.%M.%s')
-				ouput.pt_csv(judul_blank,hasil)
-			else:
-				ouput.pt_csv(judul_opsi,hasil)
-		
-		elif opsi == '4':
-			"""cetak ke text"""
-			judul_opsi = input("beri judul : " )
-			if judul_opsi == None:
-				judul_blank = dt.today.strftime('%Y-%m-%d-%H.%M.%s')
-				ouput.pt_txt(judul_blank,hasil)
-			else:
-				ouput.pt_txt(judul_opsi,hasil)
-		else:
-			"""opsi diluar batas"""
-			print("\n"+"pilihan anda tidak ada dalm daftar \n\n ulangi lagi?")
+	
+		"""tanya tanggal awal dan akhir otomatis"""
+		awal = proses.get_input(' masukkan tanggal awal absensi: ') 
+		akhir = proses.get_input(' masukkan tanggal akhir absensi: ')
+		hasil = []
+	
+		"""main proses"""
+		"""proses ada di dalam modul proses"""
+		proses.process(awal,akhir,hasil)
+		"""otomatis menyimpan ke database"""
+		try:
+			output.pt_db(hasil)
 			
-			answer = input("\n\njawab Y atau N: ")
-			if answer.lower() == 'y':
-				return this_output()
-			else:
-				pass
+		except Exception:
+			print(f"gagal menyimpan ke database karena : \n{Exception}\n")
+			print(f"laporan dari system : \n{sys.exc_info()}")
+	return hasil
+
+def cetak():
+	"""memproses data dari penampung untuk di cetak ke layar atau berkas"""
 	
-	this_output()
+	cls()
+	print("""
+	pilih format output dari 5 opsi
+	 0 = layar
+	 1 = excel
+	 2 = JSON
+	 3 = csv
+	 4 = text
+	""")
+	"""pilih opsi output"""
+	opsi = str(input (" opsi : "))
 	
+	if opsi =='0':
+		"""cetak ke layar"""
+		output.pt_screen(hasil)
+	
+	elif opsi == '1':
+		"""cetak ke excell"""
+		judul_opsi = input("beri judul : " )
+		if judul_opsi == None:
+			judul_blank = dt.today.strftime('%Y-%m-%d-%H.%M.%s')
+			ouput.pt_excell(judul_blank,hasil)
+		else:
+			ouput.pt_excell(judul_opsi,hasil)
+		
+	elif opsi == '2':
+		"""cetak ke JSON"""
+		judul_opsi = input("beri judul : " )
+		if judul_opsi == None:
+			judul_blank = dt.today.strftime('%Y-%m-%d-%H.%M.%s')
+			ouput.pt_json(judul_blank,hasil)
+		else:
+			ouput.pt_json(judul_opsi,hasil)
+		
+	elif 'opsi' == '3':
+		"""cetak ke csv"""
+		judul_opsi = input("beri judul : " )
+		if judul_opsi == None:
+			judul_blank = dt.today.strftime('%Y-%m-%d-%H.%M.%s')
+			ouput.pt_csv(judul_blank,hasil)
+		else:
+			ouput.pt_csv(judul_opsi,hasil)
+	
+	elif opsi == '4':
+		"""cetak ke text"""
+		judul_opsi = input("beri judul : " )
+		if judul_opsi == None:
+			judul_blank = dt.today.strftime('%Y-%m-%d-%H.%M.%s')
+			ouput.pt_txt(judul_blank,hasil)
+		else:
+			ouput.pt_txt(judul_opsi,hasil)
+	else:
+		"""opsi diluar batas"""
+		print("\n"+"pilihan anda tidak ada dalm daftar \n\n ulangi lagi?")
+	
+		answer = input("\n\njawab Y atau N: ")
+		if answer.lower() == 'y':
+			return this_output()
+		else:
+			pass
+
+def cls():
+	"""modul untuk membersihkan layar"""
+	import os
+	os.system('cls' if os.name == 'nt' else 'clear')
+
+def keluar():
+	"""modul untuk keluar"""
+	print('Terima Kasih')
+	sys.exit
+
+def ulang():
 	"""opsi ulangi proses"""
+	cls()
 	print("\n\nulangi proses ?")
 	answer = input("\n\njawab Y atau N: ")
 	if answer.lower() == 'y':
 		return main()
 	else:
-		sys.exit
+		keluar()
+
+def main():
+	"""fungsi utama, semua data diproses disini"""
+	"""salam pembuka"""
+	print("""
+	SELAMAT DATANG DI ABTOMATIS
+	MODUL PENGISI ABSENSI OTOMATIS
+	""")
+	
+	def opsi_menu():
+		"""modul menu"""
+		cls()
+		print("""
+		1 - lihat data laporan \t\t 2 - lihat data pegawai
+		3 - lihat data jadwal \t\t 4 - lihat data liburan
+		O - ubah / hapus data laporan \t\t P - ubah / hapus data pegawai
+		J - ubah / hapus data jadwal \t\t L - ubah / hapus data liburan
+		B - buat laporan otomatis \t\t C - cetak laporan
+		M - lihat opsi \t\t Q - keluar
+		""")
+	menu()
+	
+	def opsi_input():
+		opsi = str(input("pilih opsi :\t"))
+		
+		if opsi.lower() == '1': # lihat data laporan
+			cls()
+			view_tabel('absensi')
+			opsi_input()
+		elif opsi.lower() == '2': # lihat data pegawai
+			cls()
+			view_tabel('pegawai')
+			opsi_input()
+		elif opsi.lower() == '3': # lihat data jadwal
+			cls()
+			view_tabel('jadwal')
+			opsi_input()
+		elif opsi.lower() == '4': # lihat data liburan
+			cls()
+			view_tabel('liburan')
+			opsi_input()
+		elif opsi.lower() == 'o': # ubah data laporan
+			print(opsi)
+		elif opsi.lower() == 'p': # ubah data pegawai
+			cls()
+			getdata.ubah_pegawai()
+			opsi_input()
+		elif opsi.lower() == 'j': # ubah data jadwal
+			cls()
+			getdata.ubah_jadwal()
+			opsi_input()
+		elif opsi.lower() == 'l': # ubah data liburan
+			cls()
+			getdata.ubah_liburan()
+			opsi_input()
+		elif opsi.lower() == 'b': # buat laporan otomatis
+			cls()
+			buat()
+			opsi_input()
+		elif opsi.lower() == 'c': # cetak laporan
+			cls()
+			cetak()
+			opsi_input()
+		elif opsi.lower() == 'q': # keluar
+			cls()
+			keluar()
+		elif opsi.lower() == 'm': # lihat menu
+			cls()
+			opsi_input()
+		else: # opsi tidak dikenali
+			print(f'opsi {opsi} tidak ditemukan, ulangi?\n')
+			ulang()
+	
+	opsi_input()
 	conn.close()
 
 if __name__=='__main__':
-	main()
+main()
